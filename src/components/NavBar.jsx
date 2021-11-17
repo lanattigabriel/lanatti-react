31628f8e604286d5db54bbabed73d5255b4534cb
lanattigabriel <lanattigabriel@gmail.com>
2021-11-16 22:04:57 -0300

    navBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './EstilosNavBar.scss'
+function NavBar() {
+    return(
+        <nav className="nav">
+            <div className="logo">
+                <h2 className="logo__title">Brand</h2>
+            </div>
+            <ul className="nav__ul">
+                <li className="nav__li">
+                    <a className="nav__a" href="/">Uno</a>
+                </li>
+                <li className="nav__li">
+                    <a className="nav__a" href="/">Dos</a>
+                </li>
+                <li className="nav__li">
+                    <a className="nav__a" href="/">Tres</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
